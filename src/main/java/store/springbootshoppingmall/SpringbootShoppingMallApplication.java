670d939b73b230e31f57a2b378739710a05a1799
장동건 <wer4886@naver.com>
2024-02-29 15:48:00 +0900

    Initial commit

---
+++ src/main/java/store/springbootshoppingmall/SpringbootShoppingMallApplication.java
@@ -0,0 +1,13 @@
+package store.springbootshoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShoppingMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootShoppingMallApplication.class, args);
+	}
+
+}
